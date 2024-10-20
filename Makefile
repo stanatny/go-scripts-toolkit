aeb456ca05498c717b7ab719ec528d9d6419770c
单硕 <shanshuo@bytedance.com>
2024-10-20 21:13:53 +0800

    feat: 初始化

---
+++ Makefile
@@ -0,0 +1,67 @@
+src = $(shell find . -type f -name '*.go' | grep -v "kitex_gen" | grep -v "vendor")
+
+.PHONY: fmt build test clean
+.PHONY: run-docker-kibana run-docker-kibana-arm64 run-docker-es run-docker-es-arm64 run-docker-mysql run-docker-mysql-arm64 run-docker-mongo run-docker-mongo-arm64 run-docker-redis run-docker-redis-arm64
+.PHONY: script-gen-es-data
+
+################################################################
+# Go
+################################################################
+
+build:
+	@echo "Building Go scripts..."
+	go build -o bin/my-script ./cmd/...
+
+test:
+	@echo "Running tests..."
+	go test ./...
+
+clean:
+	@echo "Cleaning up..."
+	rm -rf bin/*
+
+fmt:
+	@echo "Formatting go files..."
+	@goimports -w $(src)
+
+################################################################
+# Env
+################################################################
+
+run-docker-es:
+	@docker run --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -d docker.elastic.co/elasticsearch/elasticsearch:7.10.0
+
+run-docker-es-arm64:
+	@docker run --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -d docker.elastic.co/elasticsearch/elasticsearch:7.17.0-arm64
+
+run-docker-kibana:
+	@docker run --name kibana --link=elasticsearch:test -p 5601:5601 -d docker.elastic.co/kibana/kibana:7.10.0
+
+run-docker-kibana-arm64:
+	@docker run --name kibana --link=elasticsearch:test -p 5601:5601 -d docker.elastic.co/kibana/kibana:7.17.0-arm64
+
+run-docker-mysql:
+	@docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql
+
+run-docker-mysql-arm64:
+	@docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 arm64v8/mysql
+
+run-docker-mongo:
+	@docker run -d --name=mongo -p 27017:27017 mongo:latest
+
+run-docker-mongo-arm64:
+	@docker run -d --name=mongo -p 27017:27017 arm64v8/mongo:latest
+
+run-docker-redis:
+	@docker run -d --name=redis -p 6379:6379 redis:latest
+
+run-docker-redis-arm64:
+	@docker run -d --name=redis -p 6379:6379 arm64v8/redis:latest
+
+################################################################
+# Scripts
+################################################################
+
+script-gen-es-data:
+	@go run cmd/es-data-loader/main.go
+
---
+++ scripts/shell/git_pull.sh
@@ -0,0 +1,96 @@
+# 主函数
+main() {
+    dest_path=$1
+    if [ -z $dest_path ]; then
+        dest_path="."
+    fi
+
+    root_path=$(
+        cd $dest_path
+        pwd
+    )
+    cd $root_path
+    for dir in $(ls $root_path); do
+        if [ -f $dir ]; then
+            echo "$dir is a file"
+            continue
+        fi
+
+        if [ ! -d $dir/.git ]; then
+            echo "$dir is not git project"
+            continue
+        fi
+
+        cd $dir
+
+        current_branch=$(git branch | grep '^\*' | cut -d' ' -f2)
+        echo "curent branch is $current_branch"
+        is_master=false
+        if [ "$current_branch" == "master" ]; then
+            is_master=true
+        fi
+        did_stashed=false
+
+        echo "========================================================"
+        git remote -v
+        echo
+
+        echo "--------------------------------------------------------"
+        echo "* git stash"
+        echo
+        stash_count_1=$(git stash list | wc -l)
+        git add .
+        git stash
+        stash_count_2=$(git stash list | wc -l)
+        if [ $stash_count_1 != $stash_count_2 ]; then
+            did_stashed=true
+        fi
+        echo
+
+        if ! $is_master; then
+            echo "--------------------------------------------------------"
+            echo "* checkout master branch"
+            echo
+            git checkout master
+            echo
+
+        fi
+
+        echo "--------------------------------------------------------"
+        echo "* git pull"
+        echo
+        git pull
+        echo
+
+        echo "--------------------------------------------------------"
+        echo "* prune remote branch"
+        echo
+        git remote prune origin
+        echo
+
+        if ! $is_master; then
+            echo "--------------------------------------------------------"
+            echo "* recover $current_branch"
+            echo
+            git checkout $current_branch
+            echo
+        fi
+
+        if $did_stashed; then
+            echo "--------------------------------------------------------"
+            echo "* recover commits"
+            echo
+            git stash pop
+            git reset
+            echo
+        fi
+
+        echo "========================================================"
+        echo
+
+        cd ..
+    done
+}
+
+## Execute main function
+main $*
